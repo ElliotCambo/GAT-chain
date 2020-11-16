125e04476221febc811391012ae15771dd7f7264
Elliot Campbelton <e.b.campbelton@gmail.com>
2020-11-16 20:10:43 +0000

    d

--- testchain.js
+++ testchain.js
@@ -219,16 +219,12 @@ app.post("/addNewIdenity", (req, res) => {
     new CryptoIdenityBlock(cid, seconds, JSON.stringify(req.body.idenity))
   );
 
-
-
   var lastBlock=GATAccountsChain.obtainLatestBlock();
   var cid = lastBlock.index;
            
   GATAccountsChain.addNewBlock(
     new CryptoAccountBlock(cid, seconds, JSON.stringify(req.body.account))
   );
-
-
 
   wss.clients.forEach(client => {
       if (client != wss) {
@@ -239,7 +235,7 @@ app.post("/addNewIdenity", (req, res) => {
   console.log(JSON.stringify(GATidenityChain));
 
   // res.sendStatus(200);
-   res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({action:"updateChain",identityChain:GATidenityChain,accountChain:GATAccountsChain,transactionChain:GATTransacitonChain}));
 
 });
