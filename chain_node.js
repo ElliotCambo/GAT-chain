f14b86a659f09b752deba3a40ee7c356a0a118c0
Elliot Campbelton <e.b.campbelton@gmail.com>
2021-10-10 14:40:21 +0100

    d

--- chain_node.js
+++ chain_node.js
@@ -95,7 +95,7 @@ app.get("/createCoins", (req, res) => {
   var pk;
   var sk;
   var trans = [];
-  var numToMint = 50;
+  var numToMint = 5000000;
   fs.readFile('/root/gat/GAT-chain/models/token.json', function(err, data) {
       transactionTemp = JSON.parse(data);
 
